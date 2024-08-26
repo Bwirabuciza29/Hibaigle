c0a72b0db63a82cbd5f1e11597d6588d9c043ba7
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2024-08-26 09:15:27 +0200

    Goingon

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,55 +1,37 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header>
-      <q-toolbar class="bg-white">
-        <q-toolbar-title>
-          <q-img class="q-pt-xs" src="~assets/hib.png" style="width: 90px" />
-        </q-toolbar-title>
-        <div class="gt-sm">
-          <q-btn class="q-mr-xs" flat color="primary" label="Course" />
-          <q-btn
-            class="q-mr-xs"
-            outline
-            rounded
-            color="primary"
-            label="Sing up"
-          />
-          <q-btn outline rounded color="primary" label="Sing in" />
-        </div>
-        <div class="lt-md">
-          <q-btn
-            color="primary"
-            flat
-            dense
-            round
-            @click="leftDrawerOpen = !leftDrawerOpen"
-            icon="menu"
-            aria-label="menu"
-          />
-        </div>
-      </q-toolbar>
-    </q-header>
-    <q-page-container>
-      <div class="q-pa-md">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id earum
-        doloremque quam excepturi culpa impedit mollitia iste ratione,
-        perferendis modi doloribus libero, harum, cumque explicabo rem dolorem
-        autem. Voluptate, adipisci.
-      </div>
-    </q-page-container>
-  </q-layout>
+  <q-page>
+    <q-toolbar
+      class="bg-transparent fixed-top"
+      :class="{ 'bg-white': scrolled }"
+    >
+      <q-avatar rounded>
+        <img src="your-logo.png" />
+      </q-avatar>
+      <q-space />
+      <q-btn color="primary" label="Button" />
+    </q-toolbar>
+
+    <div class="q-pa-md">
+      <img
+        src="your-background-image.jpg"
+        class="absolute inset-0 object-cover"
+      />
+    </div>
+  </q-page>
 </template>
 
 <script>
-import { defineComponent } from "vue";
-import { ref } from "vue";
-
-export default defineComponent({
-  setup() {
+export default {
+  data() {
     return {
-      slide: ref(1),
-      autoplay: ref(true),
+      scrolled: false,
     };
   },
-});
+
+  mounted() {
+    window.addEventListener("scroll", () => {
+      this.scrolled = window.scrollY > 0;
+    });
+  },
+};
 </script>
