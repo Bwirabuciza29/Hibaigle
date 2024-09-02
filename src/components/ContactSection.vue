8815eec86fd80ffb4b0be907aa9bf6c817e08480
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2024-09-02 14:31:59 +0200

    LetsContact

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -1,8 +1,10 @@
 <template>
   <section class="rm-container section" id="contact" data-aos="fade-up">
-    <div class="bg-sky-white bg-opacity-5 border-r border-l border-gray-400">
+    <div
+      class="bg-sky-white bg-opacity-5 border-r border-l border-gray-400 relative"
+    >
       <h2
-        class="text-4xl text-sky-900 font-bold mb-6 text-center relative transition-all duration-500 ease-in-out transform hover:text-sky-700 hover:scale-105 hover:-translate-y-1"
+        class="text-4xl text-sky-900 font-bold mb-6 ml-48 text-center relative transition-all duration-500 ease-in-out transform hover:text-sky-700 hover:scale-105 hover:-translate-y-1"
         @mouseover="hovered = true"
         @mouseleave="hovered = false"
       >
@@ -10,99 +12,48 @@
           >Contact</span
         >
       </h2>
-      <div class="grid grid-cols-1 md:grid-cols-4 gap-4 p-8 relative">
-        <!-- Large Card on the Left -->
+      <div class="container px-5 py-8 mx-auto flex">
         <div
-          class="col-span-1 md:col-span-2 bg-sky-900 text-white rounded-xl p-8 relative bg-cover bg-center"
-          style="background-image: url('/src/assets/met.jpg')"
+          class="lg:w-1/3 md:w-1/2 bg-sky-900 p-4 flex flex-col md:ml-auto w-full mt-8 md:mt-0 relative z-10 shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out"
         >
-          <div
-            class="absolute inset-0 bg-gradient-to-r from-sky-900 to-transparent opacity-80 rounded-xl"
-          ></div>
-          <div class="relative z-10">
-            <span class="block text-sm uppercase">Notre Méthodologie</span>
-            <h2
-              class="text-xl sm:text-2xl text-white pt-8 sm:pt-12 font-semibold my-2 sm:my-4 transition-transform transform hover:scale-90"
-            >
-              Nos solutions sont conçues dans le but d'offrir aux utilisateurs
-              une expérience unique et une grande facilité dans l'utilisation.
-            </h2>
-            <div
-              class="bottom-8 left-8 flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4"
-            >
-              <a
-                href="#"
-                class="relative bg-sky-700 hover:bg-sky-800 text-white py-2 px-4 rounded-lg transition-colors duration-300 transform hover:scale-105 hover:bg-gradient-to-r hover:from-sky-700 hover:to-sky-900 flex items-center justify-center"
-              >
-                Nos projets <span aria-hidden="true">→</span>
-              </a>
-            </div>
-          </div>
-        </div>
-
-        <!-- Cards on the Right -->
-        <div
-          class="col-span-1 md:col-span-2 grid grid-cols-1 md:grid-cols-2 gap-4"
-        >
-          <div
-            class="col-span-1 bg-yellow-100 text-gray-800 rounded-xl p-8 transition-transform transform hover:scale-105 hover:bg-yellow-200 relative"
+          <h2
+            class="text-white text-xl mb-2 font-semibold title-font transition-transform transform hover:translate-x-2 hover:text-sky-200"
           >
-            <!-- <img
-              src="~assets/communication.svg"
-              alt="Watermark"
-              class="absolute left-0 top-0 w-full h-full object-cover opacity-10"
-            /> -->
-            <div class="text-yellow-500 font-bold uppercase text-xs mb-2">
-              Communication
-            </div>
-            <p class="mb-4 text-justify">
-              Notre service client est à votre écoute 24h/24h et tous le jours
-              de la semaine.
-            </p>
-          </div>
-
-          <div
-            class="col-span-1 bg-red-100 text-gray-800 rounded-xl p-8 transition-transform transform hover:scale-105 hover:bg-red-200 relative"
-          >
-            <div class="text-red-500 font-bold uppercase text-xs mb-2">
-              Développement
-            </div>
-            <p class="mb-4 text-justify">
-              Nos développeurs travaillent dur pour vous assurer une meilleure
-              expérience utilisateur.
-            </p>
-
-            <!-- Watermark Image -->
-            <img
-              src="~assets/Other.png"
-              alt="Filigrane"
-              class="absolute -top-24 -right-10 w-40 h-auto opacity-100"
+            Nous contacter
+          </h2>
+          <p class="leading-relaxed mb-5 text-sky-300">
+            Post-ironic Portland shabby chic echo park, banjo fashion axe.
+          </p>
+          <div class="relative mb-4">
+            <label for="email" class="leading-7 text-sm text-sky-200">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              class="w-full bg-gray-800 rounded border border-gray-700 focus:border-sky-500 focus:ring-2 focus:ring-sky-400 text-base outline-none text-white py-2 px-3 leading-8 transition-colors duration-300 ease-in-out hover:bg-gray-700"
             />
           </div>
-
-          <div
-            class="col-span-1 bg-green-100 text-gray-800 rounded-xl p-8 transition-transform transform hover:scale-105 hover:bg-green-200 relative"
-          >
-            <div class="text-green-500 font-bold uppercase text-xs mb-2">
-              Motivation
-            </div>
-            <p class="mb-4 text-justify">
-              Motivés par la volonté de bien faire et de vous offrir le
-              meilleur.
-            </p>
+          <div class="relative mb-4">
+            <label for="message" class="leading-7 text-sm text-sky-200">
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              class="w-full bg-gray-800 rounded border border-gray-700 focus:border-sky-500 focus:ring-2 focus:ring-sky-400 h-32 text-base outline-none text-white py-2 px-3 resize-none leading-6 transition-colors duration-300 ease-in-out hover:bg-gray-700"
+            ></textarea>
           </div>
-
-          <div
-            class="col-span-1 bg-purple-100 text-gray-800 rounded-xl p-8 transition-transform transform hover:scale-105 hover:bg-purple-200 relative"
+          <button
+            class="text-white bg-sky-500 border-0 py-2 px-6 focus:outline-none hover:bg-sky-600 rounded-lg text-lg transition-transform transform hover:scale-105 duration-300 ease-in-out"
           >
-            <div class="text-purple-500 font-bold uppercase text-xs mb-2">
-              Sécurité
-            </div>
-            <p class="mb-4 text-justify">
-              La confidentialité et le chiffrement de vos données de bout en
-              bout.
-            </p>
-          </div>
+            Button
+          </button>
+          <p class="text-xs text-sky-400 mt-3">
+            Chicharrones blog helvetica normcore iceland tousled brook viral
+            artisan.
+          </p>
         </div>
       </div>
     </div>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -47,7 +47,20 @@
     <div class="rm-wrapper bg-white">
       <TeamSection />
     </div>
-    <div class="rm-wrapper">
+    <div class="rm-wrapper relative">
+      <div class="absolute inset-0 bg-gray-300">
+        <iframe
+          width="100%"
+          height="100%"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+          title="map"
+          scrolling="no"
+          src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed"
+          style=""
+        ></iframe>
+      </div>
       <ContactSection />
     </div>
     <div class="rm-wrapper bg-sky-950">
