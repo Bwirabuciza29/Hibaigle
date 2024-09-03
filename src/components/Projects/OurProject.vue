88f62a33bc9aa7a9283afb1cf6571373e8ecc227
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2024-09-03 15:32:22 +0200

    GoAgain

--- src/components/Projects/OurProject.vue
+++ src/components/Projects/OurProject.vue
@@ -3,30 +3,5 @@
     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias unde facilis
     magnam illo eius debitis velit, sint in natus illum commodi non quae
     provident vitae, ducimus accusantium quam totam aspernatur? Lorem ipsum,
-    dolor sit amet consectetur adipisicing elit. Alias unde facilis magnam illo
-    eius debitis velit, sint in natus illum commodi non quae provident vitae,
-    ducimus accusantium quam totam aspernatur? Lorem ipsum, dolor sit amet
-    consectetur adipisicing elit. Alias unde facilis magnam illo eius debitis
-    velit, sint in natus illum commodi non quae provident vitae, ducimus
-    accusantium quam totam aspernatur? Lorem ipsum, dolor sit amet consectetur
-    adipisicing elit. Alias unde facilis magnam illo eius debitis velit, sint in
-    natus illum commodi non quae provident vitae, ducimus accusantium quam totam
-    aspernatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias
-    unde facilis magnam illo eius debitis velit, sint in natus illum commodi non
-    quae provident vitae, ducimus accusantium quam totam aspernatur? Lorem
-    ipsum, dolor sit amet consectetur adipisicing elit. Alias unde facilis
-    magnam illo eius debitis velit, sint in natus illum commodi non quae
-    provident vitae, ducimus accusantium quam totam aspernatur? Lorem ipsum,
-    dolor sit amet consectetur adipisicing elit. Alias unde facilis magnam illo
-    eius debitis velit, sint in natus illum commodi non quae provident vitae,
-    ducimus accusantium quam totam aspernatur? Lorem ipsum, dolor sit amet
-    consectetur adipisicing elit. Alias unde facilis magnam illo eius debitis
-    velit, sint in natus illum commodi non quae provident vitae, ducimus
-    accusantium quam totam aspernatur? Lorem ipsum, dolor sit amet consectetur
-    adipisicing elit. Alias unde facilis magnam illo eius debitis velit, sint in
-    natus illum commodi non quae provident vitae, ducimus accusantium quam totam
-    aspernatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias
-    unde facilis magnam illo eius debitis velit, sint in natus illum commodi non
-    quae provident vitae, ducimus accusantium quam totam aspernatur?
   </div>
 </template>
--- src/pages/Projects/SectionProject.vue
+++ src/pages/Projects/SectionProject.vue
@@ -1,12 +1,134 @@
 <template>
   <div>
-    <OurProject />
-  </div>
-  <div class="rm-wrapper bg-sky-950">
-    <FooterSection />
+    <NavAbout />
+    <main class="profile-page">
+      <section class="relative block" style="height: 500px">
+        <div
+          class="absolute top-0 w-full h-full bg-center bg-cover"
+          style="background-image: url('/src/assets/a.jpg')"
+        >
+          <span
+            id="blackOverlay"
+            class="w-full h-full absolute opacity-50 bg-black"
+          ></span>
+        </div>
+      </section>
+      <section class="relative py-16 bg-slate-950">
+        <div class="container mx-auto px-4">
+          <div
+            class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64"
+          >
+            <div class="py-4 px-6 bg-white">
+              <h1 class="text-2xl font-bold mb-4">Nos Projets</h1>
+              <div class="flex flex-wrap gap-2 mb-4">
+                <a
+                  v-for="button in buttons"
+                  :key="button.id"
+                  :class="button.class"
+                  class="px-4 py-2"
+                >
+                  {{ button.label }}
+                </a>
+              </div>
+            </div>
+            <!-- Cards -->
+            <div class="p-6">
+              <div class="grid grid-cols-2 sm:grid-cols-2 gap-4">
+                <div
+                  v-for="item in cards"
+                  :key="item.id"
+                  class="bg-sky-50 flex flex-col sm:flex-row"
+                >
+                  <img
+                    :src="item.image"
+                    alt="Card Image"
+                    class="w-full sm:w-1/3 h-40 object-cover"
+                  />
+                  <div class="w-full sm:w-2/3 p-3">
+                    <div class="flex items-center gap-2 mb-1">
+                      <span
+                        :class="item.labelClass"
+                        class="px-2 py-1 text-xs font-bold rounded-full"
+                        >{{ item.label }}</span
+                      >
+                      <span class="text-gray-500 text-xs flex items-center">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="currentColor"
+                          class="w-4 h-4 mr-1"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M12 6v6l4 2"
+                          />
+                        </svg>
+                        {{ item.readTime }}
+                      </span>
+                    </div>
+                    <h2 class="text-sm font-semibold mb-1">{{ item.title }}</h2>
+                    <p class="text-xs text-gray-500">{{ item.date }}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <FooterSection />
+      </section>
+    </main>
   </div>
 </template>
 <script setup>
-import OurProject from "src/components/Projects/OurProject.vue";
+import NavAbout from "src/components/AboutSections/NavAbout.vue";
 import FooterSection from "src/components/FooterSection.vue";
+const buttons = [
+  { id: 1, label: "TOUS LES PROJETS", class: "bg-sky-800 text-white" },
+  { id: 2, label: "h~mahanay", class: "bg-gray-100 text-gray-600" },
+  { id: 3, label: "h~muke", class: "bg-gray-100 text-gray-600" },
+  { id: 4, label: "h~soma", class: "bg-gray-100 text-gray-600" },
+  { id: 5, label: "h~santé", class: "bg-gray-100 text-gray-600" },
+];
+
+const cards = [
+  {
+    id: 1,
+    image: "src/assets/met.jpg",
+    label: "h~mahanay",
+    labelClass: "bg-sky-600 text-white",
+    title: "La plateforme la plus optimale pour la gestion saine de vos biens",
+    date: "19 octobre 2023 à 11:47 UTC+2",
+    readTime: "1 min de lecture",
+  },
+  {
+    id: 2,
+    image: "src/assets/met.jpg",
+    label: "h~muke",
+    labelClass: "bg-green-600 text-white",
+    title: "Le discours du Président Macron au Forum des industries...",
+    date: "19 octobre 2023 à 11:47 UTC+2",
+    readTime: "1 min de lecture",
+  },
+  {
+    id: 3,
+    image: "src/assets/met.jpg",
+    label: "h~soma",
+    labelClass: "bg-yellow-400 text-white",
+    title: "Le discours du Président Macron au Forum des industries...",
+    date: "19 octobre 2023 à 11:47 UTC+2",
+    readTime: "1 min de lecture",
+  },
+  {
+    id: 4,
+    image: "src/assets/met.jpg",
+    label: "h~santé",
+    labelClass: "bg-purple-600 text-white",
+    title: "Le discours du Président Macron au Forum des industries...",
+    date: "19 octobre 2023 à 11:47 UTC+2",
+    readTime: "1 min de lecture",
+  },
+];
 </script>
