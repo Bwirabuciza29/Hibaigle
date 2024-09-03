b3e1ca1038780b8a76d7a30545a3474bdf5c9d19
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2024-09-03 22:28:04 +0200

    TheLastDance

--- src/components/Projects/OurProject.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="rm-container">
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias unde facilis
-    magnam illo eius debitis velit, sint in natus illum commodi non quae
-    provident vitae, ducimus accusantium quam totam aspernatur? Lorem ipsum,
-  </div>
-</template>
