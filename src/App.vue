142f5554fbd23777a8b9c7b774ff2bb11acec62b
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2024-11-02 20:52:19 +0200

    WeAreTheWorld

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,17 @@
 import { createI18n } from "vue-i18n";
 
 const i18n = createI18n({
-  // options pour vue-i18n ici ...
+  lazy: true,
+  langDir: "locales",
+  strategy: "prefix_except_default",
+  locales: [
+    { code: "fr-FR", iso: "fr-FR", name: "français", file: "fr-FR.json" },
+    { code: "en-UK", iso: "en-UK", name: "english (UK)", file: "en-UK.json" },
+    { code: "es-ES", iso: "es-ES", name: "español", file: "es-ES.json" },
+    { code: "in-HI", iso: "in-HI", name: "हिन्दी", file: "in-HI.json" },
+  ],
+  defaultLocale: "fr-FR",
+  vueI18n: "./i18n.config.js",
 });
 </script>
 
