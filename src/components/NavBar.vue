f14021ba71fff1f8d6f4db6ef945d6b65d53fae9
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2024-11-12 10:39:45 +0200

    LetsShineAganin

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -43,129 +43,20 @@
         <!-- Les boutons left du Navbar -->
         <div class="gt-sm flex flex-wrap items-center gap-2">
           <!-- <LangSwitcher class="flex-shrink-0" /> -->
-
-          <button
-            @click="openDialogue"
-            class="relative inline-block px-4 py-2 font-semibold text-white bg-sky-900 border-2 border-sky-800 rounded overflow-hidden group"
-          >
-            <span
-              class="relative z-10 transition-colors duration-300 ease-in-out group-hover:text-sky-900"
+          <router-link to="/souscrire">
+            <button
+              class="relative inline-block px-4 py-2 font-semibold text-white bg-sky-900 border-2 border-sky-800 rounded overflow-hidden group"
             >
-              Souscrire
-            </span>
-            <div
-              class="absolute inset-0 w-full h-full bg-white transform translate-x-full group-hover:translate-x-0 transition-transform duration-300 ease-in-out"
-            ></div>
-          </button>
-
-          <q-dialog v-model="dialogue" :backdrop-filter="backdropFilter">
-            <q-card
-              class="max-w-lg mx-auto overflow-y-auto hide-scrollbar bg-gray-100 square"
-              style="max-height: 90vh"
-            >
-              <q-linear-progress :value="0.6" color="light-blue-10" />
-              <q-card-section class="row items-center q-pb-none text-h6">
-                <h2 class="font-bold text-sky-700">
-                  Formulaire de souscription
-                </h2>
-              </q-card-section>
-              <hr class="border border-sky-700" />
-
-              <!-- FORMULAIRE DE SOUMISSION DES DONNEES -->
-              <q-card-section>
-                <form @submit.prevent="submitForm">
-                  <q-input
-                    dense
-                    filled
-                    v-model="nom"
-                    label="Nom"
-                    :rules="[(val) => !!val || 'Nom requis']"
-                  />
-                  <q-input
-                    dense
-                    filled
-                    v-model="nomEntreprise"
-                    label="Nom Entreprise"
-                    :rules="[(val) => !!val || 'Nom Entreprise requis']"
-                  />
-                  <q-input
-                    dense
-                    filled
-                    v-model="formeJudiciaire"
-                    label="Forme Judiciaire"
-                    :rules="[(val) => !!val || 'Forme Judiciaire requise']"
-                  />
-                  <q-input
-                    dense
-                    filled
-                    v-model="email"
-                    label="Email"
-                    :rules="[(val) => !!val || 'Email requis']"
-                  />
-                  <q-input
-                    dense
-                    filled
-                    v-model="services"
-                    label="Services"
-                    :rules="[(val) => !!val || 'Services requis']"
-                  />
-                  <q-input
-                    dense
-                    filled
-                    v-model="objectif"
-                    label="Objectif"
-                    :rules="[(val) => !!val || 'Objectif requis']"
-                  />
-                  <q-input
-                    dense
-                    filled
-                    v-model="delai"
-                    label="Delai"
-                    :rules="[(val) => !!val || 'Delai requis']"
-                  />
-                  <q-input
-                    dense
-                    filled
-                    v-model="budget"
-                    label="Budget"
-                    :rules="[(val) => !!val || 'Budget requis']"
-                  />
-                  <q-input
-                    dense
-                    filled
-                    v-model="adresse"
-                    label="Adresse"
-                    :rules="[(val) => !!val || 'Adresse requise']"
-                  />
-                  <q-input
-                    dense
-                    filled
-                    v-model="telephone"
-                    label="Téléphone"
-                    :rules="[(val) => !!val || 'Téléphone requis']"
-                  />
-                  <q-input
-                    dense
-                    filled
-                    v-model="cahierCharge"
-                    label="Cahier Charge"
-                    :rules="[(val) => !!val || 'Cahier Charge requis']"
-                  />
-                </form>
-              </q-card-section>
-              <!-- FIN FORMULAIRE DE SOUMISSION DES DONNEES -->
-              <q-card-actions align="right">
-                <q-btn
-                  square
-                  class="bg-sky-900 w-full"
-                  label="Soumettre"
-                  color="primary"
-                  :disabled="!formIsValid"
-                  @click="submitForm"
-                />
-              </q-card-actions>
-            </q-card>
-          </q-dialog>
+              <span
+                class="relative z-10 transition-colors duration-300 ease-in-out group-hover:text-sky-900"
+              >
+                Souscrire
+              </span>
+              <div
+                class="absolute inset-0 w-full h-full bg-white transform translate-x-full group-hover:translate-x-0 transition-transform duration-300 ease-in-out"
+              ></div>
+            </button>
+          </router-link>
         </div>
 
         <!-- fin bouton -->
@@ -235,18 +126,20 @@
                       </a>
                     </div>
                   </nav>
-                  <button
-                    @click="
-                      () => {
-                        openDialogue();
-                        closeMenu();
-                      }
-                    "
-                    v-close-popup
-                    class="w-full mt-4 px-4 py-2 font-semibold text-white bg-sky-900 border-2 border-sky-800 rounded"
-                  >
-                    Souscrire
-                  </button>
+                  <router-link to="/souscrire">
+                    <button
+                      @click="
+                        () => {
+                          openDialogue();
+                          closeMenu();
+                        }
+                      "
+                      v-close-popup
+                      class="w-full mt-4 px-4 py-2 font-semibold text-white bg-sky-900 border-2 border-sky-800 rounded"
+                    >
+                      Souscrire
+                    </button>
+                  </router-link>
                   <div
                     class="flex flex-col lg:flex-row justify-between items-center lg:items-start my-8 text-sm"
                   >
@@ -267,7 +160,7 @@
   </div>
 </template>
 <script setup>
-import { ref, onMounted, onUnmounted, computed } from "vue";
+import { ref, onMounted, onUnmounted } from "vue";
 import VueScrollTo from "vue-scrollto";
 
 const menuItems = [
@@ -283,49 +176,9 @@ const isMenuVisible = ref(true);
 const isMenuOpen = ref(false);
 const dialog = ref(false);
 const dialogue = ref(false);
-const backdropFilter = ref("grayscale(100%)");
 
 // État pour le lien actif
 const activeLink = ref("Accueil");
-
-// Champs du formulaire
-const nom = ref("");
-const nomEntreprise = ref("");
-const formeJudiciaire = ref("");
-const email = ref("");
-const services = ref("");
-const objectif = ref("");
-const delai = ref("");
-const budget = ref("");
-const adresse = ref("");
-const telephone = ref("");
-const cahierCharge = ref("");
-
-// Fonction pour vérifier si le formulaire est valide
-const formIsValid = computed(() => {
-  return (
-    nom.value &&
-    nomEntreprise.value &&
-    formeJudiciaire.value &&
-    email.value &&
-    services.value &&
-    objectif.value &&
-    delai.value &&
-    budget.value &&
-    adresse.value &&
-    telephone.value &&
-    cahierCharge.value
-  );
-});
-
-// Fonction de soumission du formulaire
-const submitForm = () => {
-  if (formIsValid.value) {
-    console.log("Formulaire soumis avec succès");
-  } else {
-    console.log("Veuillez remplir tous les champs");
-  }
-};
 
 // Fonction pour définir le lien actif
 const setActiveLink = (linkName) => {
